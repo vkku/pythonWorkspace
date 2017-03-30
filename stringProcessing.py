a5e4d4ba077de0d77dec7a292263877f4ef6c494
Vivek Kumar <vkku@users.noreply.github.com>
2017-03-31 01:10:12 +0530

    Basic String Functions

---
+++ stringProcessing.py
@@ -0,0 +1,31 @@
+str = "vkku \t vtsl"
+print(str)
+'''
+print(str.count(str))
+
+a="String string String string String"
+print(a.count("String"))
+
+print(str.index("vt"))
+
+print(str.endswith(("vtsl","vkku"),0,len(str)))
+
+print("Tabs to spaces : " + str.expandtabs(1))
+
+print(str.find("vkku"))
+
+print(str.isalnum())
+
+print(str.isalpha()) #isdigit()
+#islower(), isspace(), istitle(), isupper()
+
+
+print(str.title())
+
+print(str.partition("vtsl")) #will never #actually happen
+
+print(str.replace("vtsl","vtslSmoker"))
+'''
+#print(str)
+
+print(str.strip(""))
